eeaaf2f440f4346fac020c0f788de04b316b1bde
prince <princekm005@gmail.com>
2023-04-23 13:15:17 +0530

    first commit, handling new event requested with about:blank to be loaded in a different parentwindow

--- GettingStartedGuides/Win32_GettingStarted/HelloWebView.cpp
+++ GettingStartedGuides/Win32_GettingStarted/HelloWebView.cpp
@@ -25,7 +25,7 @@ HINSTANCE hInst;
 
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
+LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 // Pointer to WebViewController
 static wil::com_ptr<ICoreWebView2Controller> webviewController;
 
@@ -106,6 +106,7 @@ int CALLBACK WinMain(
 		nCmdShow);
 	UpdateWindow(hWnd);
 
+
 	// <-- WebView2 sample code starts here -->
 	// Step 3 - Create a single WebView within the parent window
 	// Locate the browser and set up the environment for WebView
@@ -115,7 +116,7 @@ int CALLBACK WinMain(
 
 				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
 				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-					[hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+					[hWnd, env](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
 						if (controller != nullptr) {
 							webviewController = controller;
 							webviewController->get_CoreWebView2(&webview);
@@ -127,15 +128,15 @@ int CALLBACK WinMain(
 						webview->get_Settings(&settings);
 						settings->put_IsScriptEnabled(TRUE);
 						settings->put_AreDefaultScriptDialogsEnabled(TRUE);
-						settings->put_IsWebMessageEnabled(TRUE);
+						settings->put_IsWebMessageEnabled(FALSE);
 
 						// Resize WebView to fit the bounds of the parent window
 						RECT bounds;
 						GetClientRect(hWnd, &bounds);
 						webviewController->put_Bounds(bounds);
 
-						// Schedule an async task to navigate to Bing
-						webview->Navigate(L"https://www.bing.com/");
+						// Schedule an async task to navigate to Outlook project
+						webview->Navigate(L"https://outlook.live.com/");
 
 						// <NavigationEvents>
 						// Step 4 - Navigation events
@@ -178,22 +179,120 @@ int CALLBACK WinMain(
 								return S_OK;
 							}).Get(), &token);
 
-						// Schedule an async task to add initialization script that
-						// 1) Add an listener to print message from the host
-						// 2) Post document URL to the host
-						webview->AddScriptToExecuteOnDocumentCreated(
-							L"window.chrome.webview.addEventListener(\'message\', event => alert(event.data));" \
-							L"window.chrome.webview.postMessage(window.document.URL);",
-							nullptr);
-						// </CommunicationHostWeb>
+						// Add a custom handler for new window request
+						webview->add_NewWindowRequested(
+							Callback<ICoreWebView2NewWindowRequestedEventHandler>(
+								[env](ICoreWebView2* sender,
+									ICoreWebView2NewWindowRequestedEventArgs* args) -> HRESULT {
+										// Handle window.open event here
+										LPWSTR uri;
+										args->get_Uri(&uri);
+										wchar_t const* prefix = L"about:blank";
+										if (wcslen(uri) && wcsncmp(uri, prefix, wcslen(prefix)) != 0)
+										{
+											args->put_Handled(FALSE);
+											return S_OK;
+										}
 
+										wil::com_ptr<ICoreWebView2NewWindowRequestedEventArgs> eventArgs;
+										eventArgs = args;
+										wil::com_ptr<ICoreWebView2Deferral> deferral;
+										HRESULT hrOk =  args->GetDeferral(&deferral);
+
+										if (SUCCEEDED(hrOk)) {
+
+											// Define the window class and register it
+											WNDCLASSEX wcex = {};
+											wcex.cbSize = sizeof(WNDCLASSEX);
+											wcex.lpfnWndProc = WndProc2;
+											wcex.hInstance = hInst;
+											wcex.lpszClassName = L"ProjectionClass";
+											RegisterClassEx(&wcex);
+
+											// Create the window
+											HWND hWnd = CreateWindowEx(
+												0,
+												L"ProjectionClass",
+												L"",
+												WS_OVERLAPPEDWINDOW,
+												CW_USEDEFAULT, CW_USEDEFAULT,
+												800, 600,
+												NULL,
+												NULL,
+												hInst,
+												NULL
+											);
+
+											if (true)
+											{
+												HRESULT hr = env->CreateCoreWebView2Controller(hWnd,
+													Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+														[eventArgs, hWnd, deferral](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+															if (controller) {
+
+																wil::com_ptr<ICoreWebView2> newWebView;
+																controller->get_CoreWebView2(&newWebView);
+																eventArgs->put_NewWindow(newWebView.get());
+
+																EventRegistrationToken token;
+																newWebView->add_DocumentTitleChanged(
+																	Callback<ICoreWebView2DocumentTitleChangedEventHandler>(
+																		[hWnd](ICoreWebView2* sender, IUnknown* args) -> HRESULT {
+																			LPWSTR title;
+																			sender->get_DocumentTitle(&title);
+																			SetWindowText(hWnd, title);
+																			return S_OK;
+																		})
+																	.Get(),
+																			&token);
+
+																//newWebView->add_SourceChanged(
+																//	Callback<IWebView2SourceChangedEventHandler>(
+																//	[](IWebView2WebView* sender, IWebView2SourceChangedEventArgs* args) -> HRESULT {
+																//		return S_OK;
+																//	})
+																//	.Get(),
+																//	nullptr
+																//		);
+																EventRegistrationToken token1;
+
+																newWebView->add_SourceChanged(
+																	Callback<ICoreWebView2SourceChangedEventHandler>(
+																		[hWnd](ICoreWebView2* sender, ICoreWebView2SourceChangedEventArgs* args)
+																		-> HRESULT {
+																			LPWSTR uri;
+																			sender->get_Source(&uri);
+																			PostMessage(hWnd, WM_CLOSE, 0, 0);
+																			return S_OK;
+																		})
+																	.Get(),
+																			&token1);
+
+
+																controller->AddRef();
+																SetProp(hWnd, L"Controller", controller);
+
+																controller->put_IsVisible(TRUE);
+																ShowWindow(hWnd, SW_SHOW);
+																deferral->Complete();
+																return S_OK;
+															}
+															return S_OK;
+														}).Get());
+											}
+											eventArgs->put_Handled(TRUE);
+										}
+										return S_OK;
+								})
+							.Get(),
+									nullptr);
 						return S_OK;
 					}).Get());
 				return S_OK;
 			}).Get());
 
 
-	
+
 	// <-- WebView2 sample code ends here -->
 
 	// Main message loop:
@@ -205,6 +304,34 @@ int CALLBACK WinMain(
 	}
 
 	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	ICoreWebView2Controller* controller = reinterpret_cast<ICoreWebView2Controller*>(GetProp(hWnd, L"Controller"));
+
+	switch (message)
+	{
+	case WM_SIZE:
+	case WM_WINDOWPOSCHANGED:
+		if (controller != nullptr) {
+			RECT bounds;
+			GetClientRect(hWnd, &bounds);
+			controller->put_Bounds(bounds);
+		};
+		break;
+	case WM_DESTROY:
+		if (controller)
+		{
+			controller->Release();
+		}
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
 }
 
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
